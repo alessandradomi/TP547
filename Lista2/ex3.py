304c8183abe55e0e050193172b98ca43cc834ea1
alessandradomi <99685921+alessandradomi@users.noreply.github.com>
2024-04-11 09:08:34 -0300

    Update ex3.py

--- Lista2/ex3.py
+++ Lista2/ex3.py
@@ -1,1 +1,21 @@
+import random
 
+dado = [1, 2, 3, 4, 5, 6]
+somas = []
+saldo = 1
+
+jogadas = int(input('Jogadas: '))
+
+for jogada in range(jogadas):
+    soma = random.choice(dado) + random.choice(dado) + random.choice(dado) + random.choice(dado)
+
+    if soma < 9:
+        saldo += 10
+    else:
+        saldo -= 1
+
+    if saldo == 0:
+        print('Você perdeu na ' + str(jogada + 1) + 'ª jogada.')
+        break
+
+print('Fim.\nSaldo: ' + str(saldo) + ' reais.')
