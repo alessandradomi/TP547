a01b0a135e2fd0656a6827d292b62614e82ef4c1
alessandradomi <99685921+alessandradomi@users.noreply.github.com>
2024-04-11 09:06:44 -0300

    Update ex2.py

--- Lista2/ex2.py
+++ Lista2/ex2.py
@@ -1,1 +1,23 @@
+import numpy as np
+import random
 
+N = 100000
+n = 5
+
+dado = [1, 2, 3, 4, 5, 6]
+lista_experimentos = []
+cont = 0
+
+for i in range(N):
+    vetor_valores_sorteados = np.array([])
+
+    for j in range(n):
+        vetor_valores_sorteados = np.append(vetor_valores_sorteados, random.choice(dado))
+
+    lista_experimentos.append(vetor_valores_sorteados)
+
+for experimento in lista_experimentos:
+    if np.isin(6, experimento):
+        cont += 1
+
+print('A probabilidade de se obter pelo menos um dado com 6 ao jogar 5 dados é ' + str(cont/N) + '.')
